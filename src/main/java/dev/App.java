91522afe0b542cd8ccbefc726b4dd451b07fa61b
Tommy-BOU <tommy.boubaker@free.fr>
2025-07-25 17:20:07 +0200

    commit

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,11 +1,18 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello World!");
+            String asciiArt = FigletFont.convertOneLine("Hello World!");
+            System.out.println(asciiArt);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
