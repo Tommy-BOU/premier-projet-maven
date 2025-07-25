640794628befbee024637b13bfce6e50abc97a60
Tommy-BOU <tommy.boubaker@free.fr>
2025-07-25 16:54:41 +0200

    first commit

---
+++ src/main/java/dev/tommy/App.java
@@ -0,0 +1,15 @@
+package dev.tommy;
+
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.IOException;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+        String asciiArt = FigletFont.convertOneLine("hello");
+        System.out.println(asciiArt);
+    }
+}
