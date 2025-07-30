81ec2449b7aac5748fd4903b6a6a63bcd47c89c0
Tommy-BOU <tommy.boubaker@free.fr>
2025-07-30 10:32:41 +0200

    tp 03

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -9,8 +11,14 @@ public class App {
     public static void main(String[] args) {
         try {
             System.out.println("Hello World!");
-            String asciiArt = FigletFont.convertOneLine("Hello World!");
+            String titre = ResourceBundle.getBundle("app").getString("titre");
+            String asciiArt = FigletFont.convertOneLine(titre);
             System.out.println(asciiArt);
+
+            // affichage de l'environnement
+            String environnement = ResourceBundle.getBundle("app").getString(
+                    "environnement");
+            System.out.println("Environnement : " + environnement);
         } catch (Exception e) {
             e.printStackTrace();
         }
